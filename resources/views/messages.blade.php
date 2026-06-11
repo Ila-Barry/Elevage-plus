2bb58369ea20b7c15d565ad58386818453ff769b
aichasow30 <sowaicha909@gmail.com>
2026-06-11 21:06:13 +0000

    placement du bouton de conversation

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -89,7 +89,12 @@
                     <div class="conversation-time">
                         <span>1 ja</span>
                     </div>
-                </div>
+                </div> <br> <br> <br>
+                <div class="app-banner-buttons">
+                <button class="btn-new-conversation">
+                    <i class="fas fa-plus mr-2"></i> Nouvelle conversation
+                </button>
+            </div>
             </div>
 
             <!-- Bouton Nouvelle conversation (mobile) -->
@@ -195,21 +200,6 @@
                 </div>
             </div>
 
-        </div>
-    </div>
-
-    <!-- Bannière application mobile (conforme à l'image) -->
-    <div class="app-banner">
-        <div class="app-banner-content">
-            <div class="app-banner-text">
-                <h4><i class="fas fa-mobile-alt mr-2"></i> Application mobile</h4>
-                <p>Gérez votre élevage <br>partout, à tout moment</p>
-            </div>
-            <div class="app-banner-buttons">
-                <button class="btn-new-conversation">
-                    <i class="fas fa-plus mr-2"></i> Nouvelle conversation
-                </button>
-            </div>
         </div>
     </div>
 
