fbc6b7148f4bb0be5c92f3b12721c3a5620dd694
Marietouba <marietou.ba5@unchk.edu.sn>
2026-06-17 23:03:06 +0000

    modification de la page comminauté eleveur

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -7,7 +7,7 @@
 @endpush
 
 @section('content')
-<div class="blog-main-container">
+<div class="blog-main-container p-1">
     
     <div class="blog-top-header">
         <h2 class="blog-main-title">COMMUNAUTÉ ÉLEVEURS</h2>
@@ -55,15 +55,50 @@
                     <span class="dot-indicator "></span> EXPERIENCE : NOUVELLE MÉTHODE D'ALIMENTATION
                 </h3>
                 <p class="post-text-content">
-                    Je viens de tester une nouvelle méthode d'alimentation<br> pour mes vaches et les résultats sont encourageants...
+                    Je viens de tester une nouvelle méthode d'alimentation pour mes vaches et les résultats sont encourageants...
                 </p>
                 
-                <div class="post-images-grid-2x2">
-                    <div class="grid-img-item"><img src="https://images.unsplash.com/photo-1570042225831-d98fa7577f1e?w=300" alt="Alimentation vaches 1"></div>
-                    <div class="grid-img-item"><img src="https://images.unsplash.com/photo-1546445317-29f4545e9d53?w=300" alt="Alimentation vaches 2"></div>
-                    <div class="grid-img-item"><img src="https://images.unsplash.com/photo-1596733430284-f7437764b1a9?w=300" alt="Alimentation vaches 3"></div>
-                    <div class="grid-img-item"><img src="https://images.unsplash.com/photo-1500937386664-56d1dfef3854?w=300" alt="Alimentation vaches 4"></div>
-                </div>
+               <div class="post-images-gallery">
+
+    <!-- Ligne des 4 images -->
+    <div class="image-row">
+        <!-- Image 1 -->
+        <div class="grid-img-item">
+            <img src="https://images.unsplash.com/photo-1570042225831-d98fa7577f1e?w=300" alt="Alimentation vaches 1">
+        </div>
+        <!-- Image 2 -->
+        <div class="grid-img-item">
+            <img src="https://images.unsplash.com/photo-1546445317-29f4545e9d53?w=300" alt="Alimentation vaches 2">
+        </div>
+        <!-- Image 3 -->
+        <div class="grid-img-item">
+            <img src="https://images.unsplash.com/photo-1596733430284-f7437764b1a9?w=300" alt="Alimentation vaches 3">
+        </div>
+        <!-- Image 4 = label cliquable -->
+        <div class="grid-img-item">
+            <label for="gallery-toggle-1" class="gallery-trigger">
+                <img src="https://images.unsplash.com/photo-1500937386664-56d1dfef3854?w=300" alt="Voir plus d'images">
+                <span class="overlay-plus">+</span>
+            </label>
+        </div>
+    </div>
+
+    <!-- Checkbox cachée -->
+    <input type="checkbox" id="gallery-toggle-1" class="gallery-checkbox">
+
+    <!-- Images supplémentaires (cachées par défaut) -->
+    <div class="extra-images">
+        <div class="extra-grid">
+            <img src="https://images.unsplash.com/photo-1516467508483-a7212febe31a?w=300" alt="Extra 1">
+            <img src="https://images.unsplash.com/photo-1500595046743-cd271d694d30?w=300" alt="Extra 2">
+            <img src="https://images.unsplash.com/photo-1532009877282-3340270e0529?w=300" alt="Extra 3">
+            <img src="https://images.unsplash.com/photo-1520523839897-bd0b52f945a0?w=300" alt="Extra 4">
+            <img src="https://images.unsplash.com/photo-1516467508483-a7212febe31a?w=300" alt="Extra 5">
+            <img src="https://images.unsplash.com/photo-1500595046743-cd271d694d30?w=300" alt="Extra 6">
+        </div>
+    </div>
+
+</div>
             </div>
 
             <div class="custom-post-footer">
@@ -99,15 +134,40 @@
                     <i class="fas fa-exclamation-triangle"></i> ALERTE : CAS DE FIÈVRE APHTEUSE À DAKAR
                 </h3>
                 <p class="post-text-content">
-                    Je viens de tester une nouvelle méthode d'alimentation<br> pour mes vaches et les résultats sont encourageants...
+                    Je viens de tester une nouvelle méthode d'alimentation pour mes vaches et les résultats sont encourageants...
                 </p>
                 
-                <div class="post-images-grid-2x2">
-                    <div class="grid-img-item"><img src="https://images.unsplash.com/photo-1570042225831-d98fa7577f1e?w=300" alt="Alerte bétail 1"></div>
-                    <div class="grid-img-item"><img src="https://images.unsplash.com/photo-1546445317-29f4545e9d53?w=300" alt="Alerte bétail 2"></div>
-                    <div class="grid-img-item"><img src="https://images.unsplash.com/photo-1596733430284-f7437764b1a9?w=300" alt="Alerte bétail 3"></div>
-                    <div class="grid-img-item"><img src="https://images.unsplash.com/photo-1500937386664-56d1dfef3854?w=300" alt="Alerte bétail 4"></div>
-                </div>
+                <div class="post-images-gallery">
+    <!-- Les 4 images en ligne -->
+    <div class="image-row">
+        <div class="grid-img-item"><img src="https://images.unsplash.com/photo-1570042225831-d98fa7577f1e?w=300" alt="Image 1"></div>
+        <div class="grid-img-item"><img src="https://images.unsplash.com/photo-1546445317-29f4545e9d53?w=300" alt="Image 2"></div>
+        <div class="grid-img-item"><img src="https://images.unsplash.com/photo-1596733430284-f7437764b1a9?w=300" alt="Image 3"></div>
+        
+        <!-- La 4ème image est un label -->
+        <div class="grid-img-item">
+            <label for="toggle-gallery-1" class="gallery-trigger">
+                <img src="https://images.unsplash.com/photo-1500937386664-56d1dfef3854?w=300" alt="Cliquez pour plus d'images">
+                <span class="overlay-text">+ Voir plus</span>
+            </label>
+        </div>
+    </div>
+
+    <!-- Checkbox cachée -->
+    <input type="checkbox" id="toggle-gallery-1" class="gallery-checkbox">
+
+    <!-- Conteneur d'images supplémentaires (caché par défaut) -->
+    <div class="extra-images">
+        <div class="extra-grid">
+            <img src="https://images.unsplash.com/photo-1516467508483-a7212febe31a?w=300" alt="Extra 1">
+            <img src="https://images.unsplash.com/photo-1500595046743-cd271d694d30?w=300" alt="Extra 2">
+            <img src="https://images.unsplash.com/photo-1532009877282-3340270e0529?w=300" alt="Extra 3">
+            <img src="https://images.unsplash.com/photo-1520523839897-bd0b52f945a0?w=300" alt="Extra 4">
+            <img src="https://images.unsplash.com/photo-1516467508483-a7212febe31a?w=300" alt="Extra 5">
+            <img src="https://images.unsplash.com/photo-1500595046743-cd271d694d30?w=300" alt="Extra 6">
+        </div>
+    </div>
+</div>
             </div>
 
             <div class="custom-post-footer">
@@ -144,15 +204,47 @@
                      NOUVELLE MÉTHODE D'ALIMENTATION
                 </h3>
                 <p class="post-text-content">
-    Je viens de tester une nouvelle méthode d'alimentation<br>pour mes vaches et les résultats sont encourageants...
+    Je viens de tester une nouvelle méthode d'alimentation pour mes vaches et les résultats sont encourageants...
 </p>
                 
-                <div class="post-images-grid-2x2">
-                    <div class="grid-img-item"><img src="https://images.unsplash.com/photo-1570042225831-d98fa7577f1e?w=300" alt="Conseil élevage 1"></div>
-                    <div class="grid-img-item"><img src="https://images.unsplash.com/photo-1546445317-29f4545e9d53?w=300" alt="Conseil élevage 2"></div>
-                    <div class="grid-img-item"><img src="https://images.unsplash.com/photo-1596733430284-f7437764b1a9?w=300" alt="Conseil élevage 3"></div>
-                    <div class="grid-img-item"><img src="https://images.unsplash.com/photo-1500937386664-56d1dfef3854?w=300" alt="Conseil élevage 4"></div>
-                </div>
+                <div class="post-images-gallery">
+
+    <!-- Ligne des 4 images alignées -->
+    <div class="image-row">
+        <div class="grid-img-item">
+            <img src="https://images.unsplash.com/photo-1570042225831-d98fa7577f1e?w=300" alt="Conseil élevage 1">
+        </div>
+        <div class="grid-img-item">
+            <img src="https://images.unsplash.com/photo-1546445317-29f4545e9d53?w=300" alt="Conseil élevage 2">
+        </div>
+        <div class="grid-img-item">
+            <img src="https://images.unsplash.com/photo-1596733430284-f7437764b1a9?w=300" alt="Conseil élevage 3">
+        </div>
+        <!-- La 4ème image est un label cliquable -->
+        <div class="grid-img-item">
+            <label for="gallery-1" class="gallery-trigger">
+                <img src="https://images.unsplash.com/photo-1500937386664-56d1dfef3854?w=300" alt="Voir plus">
+                <span class="overlay-plus">+</span>
+            </label>
+        </div>
+    </div>
+
+    <!-- Checkbox cachée -->
+    <input type="checkbox" id="gallery-1" class="gallery-checkbox">
+
+    <!-- Images supplémentaires (apparaissent au clic) -->
+    <div class="extra-images">
+        <div class="extra-grid">
+            <img src="https://images.unsplash.com/photo-1516467508483-a7212febe31a?w=300" alt="Extra 1">
+            <img src="https://images.unsplash.com/photo-1500595046743-cd271d694d30?w=300" alt="Extra 2">
+            <img src="https://images.unsplash.com/photo-1532009877282-3340270e0529?w=300" alt="Extra 3">
+            <img src="https://images.unsplash.com/photo-1520523839897-bd0b52f945a0?w=300" alt="Extra 4">
+            <img src="https://images.unsplash.com/photo-1516467508483-a7212febe31a?w=300" alt="Extra 5">
+            <img src="https://images.unsplash.com/photo-1500595046743-cd271d694d30?w=300" alt="Extra 6">
+        </div>
+    </div>
+
+</div>
             </div>
 
             <div class="custom-post-footer">
