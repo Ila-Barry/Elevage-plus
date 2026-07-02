57396094eaca4566a85c30f9f8a96e7d3ebe3bfb
Ilabarry <barryila20@gmail.com>
2026-07-02 09:38:21 +0000

    Fix: permissions d'exécution pour le script de build

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
+# On force la suppression d'un éventuel lien cassé avant de le recréer
+rm -rf public/storage
+
+# On installe proprement et on recrée le lien
 composer install --no-dev --optimize-autoloader
 php artisan storage:link
-mkdir -p storage/app/public/uploads/publications/images
